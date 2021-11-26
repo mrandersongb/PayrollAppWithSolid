63455262d584c5d115efda336c1d818c4a282379
Anderson <mr.anderson.brito@gmail.com>
2021-11-26 17:26:59 -0300

    [FEAT]Created Recruiter class.

---
+++ Model/Recruiter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollAppWithSolid.Model
+{
+    class Recruiter
+    {
+        const int hourlyRate = 22;
+        const int hiredBonus = 500;
+
+        public float CalculateBonus(int hoursWorked)
+        {
+            // bonus condition
+            return hoursWorked > 100 ? hiredBonus : 0;
+        }
+
+        public float CalculateTotalSalary(int hours)
+        {
+            return hours * hourlyRate + CalculateBonus(hours);
+        }
+
+    }
+}
