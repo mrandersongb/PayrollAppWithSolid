690329de29a9c672d4a2d20842c48de0e1cc478d
Anderson <mr.anderson.brito@gmail.com>
2021-11-26 16:35:10 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayrollAppWithSolid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
