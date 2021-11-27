6ce3ea4654044a850784b2002fc904871712ffab
Anderson <mr.anderson.brito@gmail.com>
2021-11-26 21:40:58 -0300

    [FEAT]Created IEmployee interface.

---
+++ Model/IEmployee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollAppWithSolid.Model
+{
+    interface IEmployee
+    {
+        float CalculateSalary(int hours);
+    }
+}
